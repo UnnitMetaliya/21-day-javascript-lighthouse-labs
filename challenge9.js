5166bbac3abdaff00f5225b3796a1f296ea992dc
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 12:56:45 -0400

    Challenge #0 - Setting the message.

---
+++ challenge9.js
@@ -0,0 +1,8 @@
+// Challenge #9: Setting the message.
+
+function setMessage() {
+  var currentNavigationState = JSON.stringify(navigation);
+  radio.message = currentNavigationState;
+}
+
+setMessage();
