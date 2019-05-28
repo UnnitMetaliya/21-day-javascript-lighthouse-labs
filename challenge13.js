777ecfd6fd24976776842efe8095428be3e00cfd
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 13:25:40 -0400

    Challenge #13 - Initializing navigation dimensions x, y, z to 0.

--- challenge13.js
+++ challenge13.js
@@ -1,4 +1,4 @@
-// Challenge #13 - Initializing navigation dimensions x, y, z to 0
+// Challenge #13 - Initializing navigation dimensions x, y, z to 0.
 
 var initialize = () => {
   navigation.x = navigation.y = navigation.z = 0;
