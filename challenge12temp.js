f907b6f4ef90ff9e4a441867e5104f81c2364d3d
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 13:54:30 -0400

    Challenge #13 - Calibrate X

--- challenge12temp.js
+++
@@ -1,5 +0,0 @@
-// Challenge #13 - Initializing navigation dimensions x, y, z to 0.
-
-var initialize = () => {
-  navigation.x = navigation.y = navigation.z = 0;
-};
---
+++ challenge13temp.js
@@ -0,0 +1,13 @@
+// Challenge #13: Calibarating X.
+
+var calibrateX = () => {
+  for (var i = 1; i <= 12; i++) {
+    var signal = checkSignal();
+    if (signal) {
+      navigation.x = signal;
+      break;
+    }
+  }
+};
+
+calibrateX();
