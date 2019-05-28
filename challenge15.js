7bc61df514e44b7532ececd36c4bd63ceaa78d87
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 13:56:05 -0400

    Challenge #14 - Calibrate Y & Z.

---
+++ challenge15.js
@@ -0,0 +1,24 @@
+// Challenge #14: Calibrating Y & Z.
+
+var calibrateY = () => {
+  for (var i = 1; i <= 60; i++) {
+    var signal = checkSignal();
+    if (signal) {
+      navigation.y = signal;
+      break;
+    }
+  }
+};
+
+var calibrateZ = () => {
+  for (var i = 1; i <= 60; i++) {
+    var signal = checkSignal();
+    if (signal) {
+      navigation.z = signal;
+      break;
+    }
+  }
+};
+
+calibrateY();
+calibrateZ();
