a62679e04895f4b7065ebb50e14736996dd75d35
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 13:00:47 -0400

    Challenge #10 - Activating Radio Beacon.

---
+++ challenge10.js
@@ -0,0 +1,6 @@
+// Challenge #9: Activating Beacon.
+// Trying new syntax for function declaration. Somehow this version is fast and looks more cool these days. See no "function" keyword.
+
+var activateBeacon = () => {
+  radio.beacon = true;
+};
