fcc1f96321d0fe7f639b8a82cb9dffd36094f307
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 13:47:53 -0400

    Challenge #12: Setting the frequency using formula.

--- challenge12.js
+++ challenge12.js
@@ -1,4 +1,4 @@
-// Challenge #11: Setting the frequency using formula.
+// Challenge #12: Setting the frequency using formula.
 
 var setFrequency = () => {
   let tuningFrequency = (radio.range.low + radio.range.high) / 2;
