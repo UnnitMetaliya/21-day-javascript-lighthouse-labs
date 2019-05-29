462255ad82a967fcf9dfa8a86ff27d76d9cb7222
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-29 00:29:05 -0400

    Challenge #20 - Decoding message.

--- challenge19.js
+++ challenge19.js
@@ -7,19 +7,3 @@ var configureBroadcast = () => {
 };
 
 configureBroadcast();
-
-function decodeMessage(message) {
-  var splittedMessage = message.split("");
-  for (var i = 0; i < splittedMessage.length; i++) {
-    splittedMessage[i] = splittedMessage[i]
-      .replace("1", "i")
-      .replace("3", "e")
-      .replace("0", "o")
-      .replace("4", "a")
-      .replace("5", "y")
-      .replace("2", "u")
-      .replace("1", "l");
-  }
-  message = splittedMessage.join("");
-  return message;
-}
