c2ed45e7a1fd6cbef6032398ec97a2b595a5665b
Unnit Metaliya <unnitjmetaliya@gmail.com>
2019-05-28 22:10:47 -0400

    Challenge #19 - Configuring Broadcast. Removing previous function calls.

---
+++ challenge19.js
@@ -0,0 +1,9 @@
+// Challenge #19 - Configuring Broadcast. Removing previous function calls.
+
+var configureBroadcast = () => {
+  setFrequency();
+  activateAntenna();
+  sendBroadcast();
+};
+
+configureBroadcast();
